6ce4a4078b0b333c674f2e931af9a637dcc34c13
sudulge <hhs0888@naver.com>
2023-10-09 14:24:02 +0900

    2-10 까지

--- archive/views.py
+++ archive/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseNotAllowed
 from django.utils import timezone
 from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 
@@ -16,11 +18,28 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    #방법 1
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    #방법 2
-    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    # answer.save()
-    return redirect('archive:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('archive:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible')
+    context = {'question': question, 'form': form}
+    return render(request, 'archive/question_detail.html', context)
 
-    
+def question_create(request):
+    if request.method == 'POST': # POST 요청 question_form.html 에서 저장하기 버튼을 클릭 했을 때
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('archive:index')
+    else: # GET 요청 question_list.html 에서 질문 등록하기 버튼을 클릭 했을 때.
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'archive/question_form.html', context)
